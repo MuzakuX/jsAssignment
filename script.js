8ed2c6b9b820d0693e9eaaadf5cc8a63beb8ee9a
MuzakuX <103932061+MuzakuX@users.noreply.github.com>
2022-07-11 12:59:37 +1000

    major updates
    almost done

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 window.onload = function(){
     let btn = document.createElement("button");
-    btn.innerText = "Search";
-    btn.type = "submit";
-    btn.name = "formBtn";
-    document.getElementById("button").appendChild(btn);
-    btn.addEventListener("click", function() {
-        displayFilm
-        console.log('success')
-    })
-fetch("https://ghibliapi.herokuapp.com/films/")
+    
+fetch("https://ghibliapi.herokuapp.com/films")
     .then((response) => {
-        if (response.ok){
+        if (response.ok) {
             return response.json();
         }
     })
     .then(data => {
         console.log(data);
-        displayFilm([1]);
+        displayFilm(data);
     })
 
     function displayFilm(data) {
-    let film = data.id[0];
+        for (let i=0; i<data.length;i++){
+            data[i]
+    const film = data[i];
     let filmDiv = document.getElementById("film");
     let filmName = film.title;
     let heading = document.createElement("h1");
     heading.innerText = filmName;
-    filmDiv.appendChild(heading);
-    
-}}
+    fData.appendChild(heading);
+    const filmImg = document.createElement("img");
+    filmImg.src = film.image;
+    fData.appendChild(filmImg);
+    const filmReleaseDate = film.release_date;
+    let paragraph = document.createElement("p");
+    paragraph.innerText = filmReleaseDate
+    fData.appendChild(paragraph);
+    let filmProducer = film.producer;
+    let heading3 = document.createElement("h3");
+    heading3.innerText = filmProducer;
+    fData.appendChild(heading3);
+    heading3.setAttribute("class", filmProducer);
+    paragraph.setAttribute("class", filmProducer);
+    heading.setAttribute("class", filmProducer);
+        }}   
+}
